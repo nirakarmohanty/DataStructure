4dffbafd77b0ac33e0d86cdebaa7ce429ee20d87
Nirakar Mohanty <nirakarmohanty@gmail.com>
2017-04-12 00:42:25 +0530

    comments in queue class

--- src/main/java/com/src/nirakar/ds/queue/Queue.java
+++ src/main/java/com/src/nirakar/ds/queue/Queue.java
@@ -2,37 +2,44 @@ package com.src.nirakar.ds.queue;
 
 import java.util.NoSuchElementException;
 
+/**
+ * Queue implementation of Java
+ * 
+ * @author NIRAKAR
+ * 
+ */
 public class Queue {
 
-	private int size;
-	private int front;
-	private int rear;
-	private int length;
-	private Object[] array;
+	private int size; // Size of the array
+	private int front;// This will be used to hold the bottom element
+	private int rear; // Pointer used in top element
+	private int length; // Length of the object array.
+	private Object[] array;// Variable used to hold the objects
 
 	public Queue(int size, Object[] array) {
 
 		this.size = size;
 		array = new Object[size];
-		this.front = -1;
-		this.rear = -1;
-		length = 0;
+		this.front = -1;// Start with -1
+		this.rear = -1; // Start with -1
+		length = 0; // And initially the length of the array is 0
 	}
 
+	// Initially the FRONT variable value is -1 , if first element enters to the
+	// array it's value will be  changed to 0 , so to check the array is empty or not , compare the value of front GT -1
 	public boolean isEmpty()
 
 	{
 		return front == -1;
-
 	}
-
+	// If the REAR variable is equal to SIZE Variable 
 	public boolean isFull()
 
 	{
 		return front == 0 && rear == size - 1;
 
 	}
-
+	// Return the length of the array
 	public int getSize()
 
 	{
